165c59ad78fa38ceb5a0afaca458c5384ce9014e
Vitalis-hub <Michealvitalis123@gmail.com>
2021-01-23 16:15:37 -0600

    JFrame window added

---
+++ Main.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+public class Main {
+
+    public static void main(String[] args){
+        
+        JFrame window = new JFrame();
+        window.setSize(500, 300);
+        window.setLocation(400,200);
+        window.pack();
+        window.setVisible(true);
+
+
+
+    }
+}
